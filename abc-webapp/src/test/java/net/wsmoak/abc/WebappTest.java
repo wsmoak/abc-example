897d45eeff8ce181558bfc15666a36a2b727954a
Wendy Smoak <wsmoak@gmail.com>
2009-03-26 10:37:56 +0100

    Add a Selenium test for the abc webapp

---
+++ abc-webapp/src/test/java/net/wsmoak/abc/WebappTest.java
@@ -0,0 +1,27 @@
+package net.wsmoak.abc;
+
+import com.thoughtworks.selenium.*;
+import java.util.regex.Pattern;
+import junit.framework.TestCase;
+
+
+public class WebappTest extends TestCase {
+
+    private Selenium selenium;
+
+	public void setUp() throws Exception {
+        selenium = new DefaultSelenium( "localhost", 4444, "*firefox", "http://localhost:8080" );
+        selenium.start();
+	}
+
+    public void tearDown() throws Exception {
+	    selenium.stop();
+    }
+
+	public void testNew() throws Exception {
+		selenium.open("http://localhost:8080/abc/");
+        Thread.sleep(5000);
+		assertTrue(selenium.isTextPresent("Hello World!"));
+        Thread.sleep(5000);
+	}
+}
